dfd22d38b859b56dec77ffa8b1b5d63c224e97a2
Radhika Bandodkar <radhika_bandodkar@ifbglobal.com>
2026-09-13 16:39:45 +0530

    Release 1.0.2: Include Swift modules in all binaries

--- Package.swift
+++ Package.swift
@@ -17,17 +17,17 @@ let package = Package(
         // Precompiled binaries with Swift modules embedded
         .binaryTarget(
             name: "IFBSDKCoreBinary",
-            url: "https://github.com/radhikaIFB/IFBApplianceSDK-Distribution/releases/download/1.0.1/IFBSDKCore.xcframework.zip",
+            url: "https://github.com/radhikaIFB/IFBApplianceSDK-Distribution/releases/download/1.0.2/IFBSDKCore.xcframework.zip",
             checksum: "85c6aeb21f5bf18ee197849da9a83d1504536b2ea5f40e7625f6e19c3a596eb6"
         ),
         .binaryTarget(
             name: "IFBProvisioningSDKBinary",
-            url: "https://github.com/radhikaIFB/IFBApplianceSDK-Distribution/releases/download/1.0.1/IFBProvisioningSDK.xcframework.zip",
+            url: "https://github.com/radhikaIFB/IFBApplianceSDK-Distribution/releases/download/1.0.2/IFBProvisioningSDK.xcframework.zip",
             checksum: "1df84e9c23d3f081d457adb8e525deb70c5a2c0fc48e1b21822a3685d9162e0a"
         ),
         .binaryTarget(
             name: "IFBApplianceSDKBinary",
-            url: "https://github.com/radhikaIFB/IFBApplianceSDK-Distribution/releases/download/1.0.1/IFBApplianceSDK.xcframework.zip",
+            url: "https://github.com/radhikaIFB/IFBApplianceSDK-Distribution/releases/download/1.0.2/IFBApplianceSDK.xcframework.zip",
             checksum: "3f7bff07cde9926154c88684659e6859f78026689e376584f1b178426d3b3e60"
         ),
 
